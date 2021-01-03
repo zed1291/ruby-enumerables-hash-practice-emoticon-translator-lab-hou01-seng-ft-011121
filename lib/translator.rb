b9b6c69814ffb9d920090e6f1ca9963d37c0ce51
zed <zed@kolk.me>
2021-01-03 01:23:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,21 @@ require 'yaml'
 
 def load_library path
   emoticons = YAML.load_file(path)
+  #this is the data
+  # happy:
+  # - ":)"
+  # - "(＾ｖ＾)"
+
+  #this is what i want
+  # {
+  #     'happy' => {
+  #        :english => ":)",
+  #        :japanese => "(＾ｖ＾)"
+  #     }
+  #  }
+  hash = {}
+  emoticons.each do |meaning, symbols|
+    hash[meaning][:english] << symbols
 end
 
 def get_japanese_emoticon
