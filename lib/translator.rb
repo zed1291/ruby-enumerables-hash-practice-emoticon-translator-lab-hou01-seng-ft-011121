5764e5c5f9227ed6b0e69ba3df545d9d324962a6
zed <zed@kolk.me>
2021-01-03 00:05:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,9 @@
 # require modules here
 require 'yaml'
-thing = YAML.load_file('.lib/emoticons.yml')
 
 def load_library
   # code goes here
+  YAML.load_file('.lib/emoticons.yml')
 end
 
 def get_japanese_emoticon
